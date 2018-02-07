3dd4e7e2c1d83d4de7904149f2937613fb1ca0d4
Charis Amarantigos <harisamarad@gmail.com>
2018-02-07 06:25:45 +0200

    Buttons and consts

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Malakas', age: 28},
+      {name: 'Trompas', age: 32},
+      {name: 'Anna', age: 22}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('success');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello!!! World????</h1>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switcher</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My hobbies are: awesome Makeup effects </Person>
       </div>
     );
   }
