e4358a1d8299964fe0c0dfa2712bc86a5c797a2e
Charis Amarantigos <harisamarad@gmail.com>
2018-02-07 07:18:51 +0200

    input gone

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -11,18 +11,44 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('success');
+  switchNameHandler = (newName) => {
+    //console.log('success');
+    // Wrong method this.state.persons[0].name= 'Malakotrampoukos';
+    this.setState({
+      persons:[
+      {name: newName, age: 28},
+      {name: 'Trompas', age: 32},
+      {name: 'Anna', age: 23}
+    ]
+   })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+      {name: 'Malakas', age: 28},
+      {name: event.target.value, age: 32},
+      {name: 'Anna', age: 23}
+    ]
+   })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hello!!! World????</h1>
-        <button onClick={this.switchNameHandler}>Switcher</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My hobbies are: awesome Makeup effects </Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Malak')}>Switcher</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} 
+          click={this.switchNameHandler.bind(this, 'Trompatatour')}
+          changed={this.nameChangedHandler}>My hobbies are: awesome Makeup effects </Person>
       </div>
     );
   }
